a448cbe8e407cc5e3cbfc53dad7ab370d8e07da5
Joe Huss <detain@interserver.net>
2017-08-10 22:26:42 -0400

    updates to the phpdoc headers removing lastchangedate and fixing the @author tag.. some other minor chagnes too

--- src/modernbill.functions.inc.php
+++ src/modernbill.functions.inc.php
@@ -1,8 +1,7 @@
 <?php
 	/**
 	 * ModernBill Related Functionality
-	 * Last Changed: $LastChangedDate: 2017-07-30 20:24:37 -0400 (Sun, 30 Jul 2017) $
-	 * @author detain
+	 * @author Joe Huss <detain@interserver.net>
 	 * @copyright 2017
 	 * @package MyAdmin
 	 * @category ModernBill
