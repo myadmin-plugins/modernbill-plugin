466ffb3dd8e9f9b30e4fbdd2376626dab32e397b
Joe Huss <detain@interserver.net>
2024-12-05 02:37:38 -0500

    update to plugins

--- src/modernbill.functions.inc.php
+++ src/modernbill.functions.inc.php
@@ -3,7 +3,7 @@
     /**
      * ModernBill Related Functionality
      * @author Joe Huss <detain@interserver.net>
-     * @copyright 2019
+     * @copyright 2025
      * @package MyAdmin
      * @category ModernBill
      */
